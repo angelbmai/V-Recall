f464a0c230ac3a77f93e5776761e0bd2c0b81b61
Angel Mai <abmai@ucsd.edu>
2023-01-15 09:53:10 -0500

    VRecall Game Code

---
+++ Assets/NextLevelScreen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelScreen : MonoBehaviour
+
+{
+
+
+    //public float restartDelay = 2f;
+    //public Text progressText;
+   // public float delayTime = 2f;
+
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+
+    }
+
+    public void DeActivate()
+    { 
+        gameObject.SetActive(false);
+    }
+
+    public void NextLevelButton()
+    {
+        SceneManager.LoadScene("livingroomScripted");
+       // gameObject.SetActive(false);
+    }
+}
